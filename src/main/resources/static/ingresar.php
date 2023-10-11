c0072ef398e6d566e292d9a98d5361fd77e9c371
lukitasruiz43 <136395331+lukitasruiz43@users.noreply.github.com>
2023-10-11 18:11:50 -0300

    dddd

--- src/main/resources/static/ingresar.php
+++ src/main/resources/static/ingresar.php
@@ -1,9 +1,10 @@
 <?php
-include("conexion.php");
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+ include("conexion.php");
 error_reporting(E_ALL);
+ini_set('display_errors', 1);
+session_start();
 ?>
+
 <!DOCTYPE html>
 <html lang="es-ES">
 <head>
@@ -34,3 +35,4 @@ error_reporting(E_ALL);
     </form>
 </body>
 </html>
+
